cdff1ca48796b2e27fc9d5516a9ee902afadb852
louie-tsai <louie.tsai@intel.com>
2026-03-08 21:55:59 -0700

    add demo deploy script

---
+++ benchmarks/cpu_binding/Demo_EP_Deploy.sh
@@ -0,0 +1,2 @@
+export MODEL=/root/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-405B-Instruct/snapshots/be673f326cab4cd22ccfef76109faf68e41aa5f1
+MODE=deploy PORT=8000 CPU_MODEL=meta-llama/Llama-3.1-8B-Instruct CPU_PORT=8001 docker compose -f docker-compose.yml -f docker-compose.cpu.yml -f docker-compose.override.yml --profile deploy up
